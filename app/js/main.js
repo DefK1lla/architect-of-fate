ef17944d28c93b5af0db6bf66de772c4a4278ba1
Sedrak Adoyan <set.adoyan@gmail.com>
2022-12-09 13:52:12 +0300

    feat: add getcourse form

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,28 @@
-function init() {
+function handlePopupOpen(e) {
+  e.preventDefault();
+
+  document.querySelectorAll(".popup").forEach(function (elem) {
+    elem.classList.remove("open");
+  });
+
+  document.querySelector(e.target.dataset.href).classList.add("open");
 
 }
 
-document.addEventListener('DOMContentLoaded', init);
+function handlePopupClose(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector(".popup.open").classList.remove("open");
+}
+
+function init() {
+  document.querySelectorAll(".popup-link").forEach(function (elem) {
+    elem.addEventListener("click", handlePopupOpen)
+  });
+
+  document.querySelectorAll(".popup-close").forEach(function (elem) {
+    elem.addEventListener("click", handlePopupClose)
+  });
+}
+
+document.addEventListener("DOMContentLoaded", init);
